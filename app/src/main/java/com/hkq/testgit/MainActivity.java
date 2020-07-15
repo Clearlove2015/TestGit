5132752f4031248694263198f7188bf00e5d5419
zhucheng <1109623941@qq.com>
2020-07-15 13:52:56 +0800

    init

---
+++ app/src/main/java/com/hkq/testgit/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hkq.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Author:zc
+ * TIME:2020-7-15 13:14:06
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("first_t");
+    }
+}
