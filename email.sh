62c1b0f29f8c64284123170d7f46c5c2c1d10bd4
zhucheng <1109623941@qq.com>
2020-07-17 10:22:03 +0800

    email.sh

---
+++ email.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_USER="other"
+OLD_EMAIL="other.qq.com"
+CORRECT_NAME="zhucheng"
+CORRECT_EMAIL="1109623941@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_USER" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
